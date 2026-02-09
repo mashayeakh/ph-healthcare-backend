77f607e9ec04c2f12b8bd2838ee8b90023e22dd1
mashayeakh <islammasayekh@gmail.com>
2026-02-10 04:04:17 +0600

    tested prisma and connected to neon db

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,6 @@
 import express, { Application, Request, Response } from 'express';
+import { prisma } from './app/lib/prisma';
+import { formatInTimeZone } from 'date-fns-tz';
 
 export const app: Application = express()
 
@@ -10,6 +12,23 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Basic route
-app.get('/', (req: Request, res: Response) => {
-    res.send('Hello, TypeScript + Express!');
+app.get('/', async (req: Request, res: Response) => {
+
+    const result = await prisma.specialty.create({
+        data: { title: "Demo one" }
+    })
+
+    //local time
+    const localCreatedAt = formatInTimeZone(result.createdAt, "Asia/Dhaka", "yyyy-MM-dd HH:mm:ss")
+    const localUpdatedAt = formatInTimeZone(result.updatedAt, "Asia/Dhaka", "yyyy-MM-dd HH:mm:ss")
+
+    res.status(201).json({
+        sucess: true,
+        message: "api is working",
+        data: {
+            ...result,
+            createdAt: localCreatedAt,
+            updatedAt: localUpdatedAt
+        }
+    })
 });
